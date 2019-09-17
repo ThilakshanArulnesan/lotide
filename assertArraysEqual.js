1c4233bea1709c6ebea96e99fd21c6b5f2aa1d22
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-09-17 13:44:20 +0000

    Added asssertArraysEqual which leveraged the pre-written eqArrays function

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+
+
+const assertEqualArrays = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅Assertion Passed ${actual} === ${expected} ✅✅`);
+  } else {
+
+    console.log(`❗️❗️Assertion Failed ${actual} !== ${expected}❗️❗️`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) return false; //Check first to see if sizes are the same
+
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+};
+
+//Test cases:
+assertEqualArrays([1, 2, 3], [1, 2, 3]); // => should PASS
+assertEqualArrays([1, 2, 3], [3, 2, 1]); // => should FAIL
+assertEqualArrays(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertEqualArrays(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
