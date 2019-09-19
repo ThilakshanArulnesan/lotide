65b56bc69cb6e5cf36cceb96750604293018dfd6
ThilakshanArulnesan <th_thilakshan@hotmail.com>
2019-09-19 17:09:57 +0000

    modified the eqObjects function to work recursively.

--- eqObjects.js
+++ eqObjects.js
@@ -13,9 +13,16 @@ const eqObjects = function (obj1, obj2) {
 
   for (let key in obj1) {
     if (obj2[key]) {
+
+
       if (Array.isArray(obj1[key])) {
         if (!Array.isArray(obj2[key])) return false; // One is an array but the other isn't
         if (!eqArrays(obj1[key], obj2[key])) return false; //Check if arrays are identitical
+      } else if (typeof obj1[key] === 'object' &&
+        typeof obj2[key] === 'object') { //arrays have already been filtered out in above if case.
+        if (!eqObjects(obj1[key], obj2[key])) {//recursive call if they are equal objects
+          return false;
+        }
       } else if (obj2[key] !== obj1[key]) { //Different values
         return false;
       }
@@ -53,3 +60,9 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+
+//New test cases for recursive eqObjects
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
